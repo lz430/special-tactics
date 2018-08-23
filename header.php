acf3ab4cdde06a8e375090a9f8e47361fd90b0df
Leonard <430designs@gmail.com>
2018-08-22 23:43:25 -0400

    Adding placeholder text for header search. Styling membership checkout page

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 		<meta name="google-site-verification" content="wR9E91XMcYFfow8XyUa6Me9RUqGVZxKkwrAu1Kqe3KU" /> 
 		
 		
-
+		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-102594572-2', 'auto');ga('send', 'pageview');</script>
 
 		<!-- Facebook Pixel Code -->
 		<script>
--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,46 @@
 // Child theme JS
 
-  $(window).ready(function(){
-    // $('.coming-soon .grid-course').each(function(){
-    //   $(this).prepend("<div class='coverage'> <h2> Coming Soon! </h2></div>")
-    // });
-  }) ;
+$(window).ready(function(){
+  filter = $('#activity-filter-select select').val();
+  $.removeCookie('bp-activity-scope', {
+      path: '/'
+  });
+  //change 'groups' with the cookie for the tab you want to be the default one
+  // favorites , friends, etc
+  $.cookie( 'bp-activity-scope', 'all');
+  scope = $.cookie('bp-activity-scope');
+  bp_activity_request(scope, filter);
 
-    $(document).ready(function(){
-      $('body.single-sfwd-courses #btn-join').val("$39.99 - Purchase This Course");
-      
+  // $('.coming-soon .grid-course').each(function(){
+  //   $(this).prepend("<div class='coverage'> <h2> Coming Soon! </h2></div>")
+  // });
+}) ;
 
-      $('.bb-user-notifications a:not(".bb-message-link") ').each(function(){
-          var linkText = $(this).text();
-          $(this).before(linkText);
-          $(this).remove();
-      });
+$(document).ready(function(){
+  $('body.single-sfwd-courses #btn-join').val("$39.99 - Purchase This Course");
+  
 
-      // Courses - watch intro replacement text
-      var textR = $('body.logged-in .course-buttons #show-video').contents();
-      // console.log(textR);
-      // textR[1].textContent="General course instructions";
+  $('.bb-user-notifications a:not(".bb-message-link") ').each(function(){
+      var linkText = $(this).text();
+      $(this).before(linkText);
+      $(this).remove();
+  });
 
-      // var pmpHeadingText = "<h1 style='width: 100%;font-size: 45px;color: #000000;text-align: center' class='vc_custom_heading'>U.S. Government Personnel</h1> <div style='width: 52%;' class='vc_separator wpb_content_element vc_separator_align_center vc_sep_width_50 vc_sep_pos_align_center vc_separator_no_text'><span class='vc_sep_holder vc_sep_holder_l'><span style='border-color:#ffb606;' class='vc_sep_line'></span></span><span class='vc_sep_holder vc_sep_holder_r'><span style='border-color:#ffb606;' class='vc_sep_line'></span></span></div>";
-      // $('#pmpro_levels_pricing_tables > .pmpro_levels_pricing_table:nth-child(2)').after(pmpHeadingText);
+  // Courses - watch intro replacement text
+  var textR = $('body.logged-in .course-buttons #show-video').contents();
+  // console.log(textR);
+  // textR[1].textContent="General course instructions";
+
+  // var pmpHeadingText = "<h1 style='width: 100%;font-size: 45px;color: #000000;text-align: center' class='vc_custom_heading'>U.S. Government Personnel</h1> <div style='width: 52%;' class='vc_separator wpb_content_element vc_separator_align_center vc_sep_width_50 vc_sep_pos_align_center vc_separator_no_text'><span class='vc_sep_holder vc_sep_holder_l'><span style='border-color:#ffb606;' class='vc_sep_line'></span></span><span class='vc_sep_holder vc_sep_holder_r'><span style='border-color:#ffb606;' class='vc_sep_line'></span></span></div>";
+  // $('#pmpro_levels_pricing_tables > .pmpro_levels_pricing_table:nth-child(2)').after(pmpHeadingText);
 
 
-      // Hide friendship accept/reject buttons when the other is clicked
-      $('.generic-button > .accept').on('click', function(){
-        $(this).parent().parent().find('.reject').hide();
-      });
+  // Hide friendship accept/reject buttons when the other is clicked
+  $('.generic-button > .accept').on('click', function(){
+    $(this).parent().parent().find('.reject').hide();
+  });
 
-      $('.generic-button > .reject').on('click', function(){
-        $(this).parent().parent().find('.accept').hide();
-      });
-
-    }) ;
+  $('.generic-button > .reject').on('click', function(){
+    $(this).parent().parent().find('.accept').hide();
+  });
+}) ;
